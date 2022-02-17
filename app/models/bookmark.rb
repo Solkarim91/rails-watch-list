f8b01cd4c88a7aa8e45919d1b4dd395894497858
Solomon Karim <solkarimlive@hotmail.co.uk>
2022-02-17 16:56:21 +0000

    finish the controller actions for the application

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
 
-  # validates [:list, :movie], uniqueness: true
   validates :comment, length: { minimum: 6 }, presence: true
   validates :movie_id, uniqueness: { scope: :list_id }
 end
